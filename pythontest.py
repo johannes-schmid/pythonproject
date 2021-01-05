69260ba8401ede49a234d6fb9c21c53611c57956
Johannes Schmid <schmid.johannes90@gmail.com>
2021-01-05 19:30:46 +0100

    commit from hansi

--- pythontest.py
+++ pythontest.py
@@ -4,11 +4,14 @@ import os
 file1 = '/Users/johannesschmid/Desktop/CodeBaking/Python Project/sheet1.xlsx'
 file2 = '/Users/johannesschmid/Desktop/CodeBaking/Python Project/sheet2.xlsx'
 
+def getExtension(file):
+    return os.path.splitext(file)[1]
+
 
 def formatcheck(file):
     # Splitting path and file extension off
     filepath = os.path.dirname(file)
-    file_extension = os.path.splitext(file)[1]
+    file_extension = getExtension(file)
 
     # Changing working directory to filepath
     os.chdir(filepath)
@@ -23,38 +26,43 @@ def formatcheck(file):
     # checking if the format is valid
     if file_extension not in allowed_extension:
         print('The following file has none supported file format:', file)
-    return file_extension
 
 formatcheck(file1)
 formatcheck(file2)
-
 
 def reading_file(file):
     filepath = os.path.dirname(file)
     # Changing working directory to filepath
     os.chdir(filepath)
     # Splitting file extension
-    file_extension = os.path.splitext(file)[1]
+    file_extension = getExtension(file)
+
 
     #Read file based on fileformat
-    if file_extension == '.xlsx' or 'xls':
+    if file_extension == '.xlsx' or file_extension == '.xls':
         # Reading excel and storing it into dataframe
         return pd.read_excel(file)
     else:
         # Reading csv and storing it into dataframe
         return pd.read_csv(file)
 
-data1 = reading_file(file1)
-data2 = reading_file(file2)
 
-def header_check(data):
+origin = reading_file(file1)
+destination = reading_file(file2)
+
+def headerCheck(data):
     #reading for 3 columns of file
-    headers = (data.columns)[:3]
+    # TODO: only check on the 3 column not the first three
+    headers = (data.columns)
+    columns = ['id', 'name', 'price']
     #checking headers on correct format
-    if not (headers==(['id', 'name', 'price'])).all():
+    for column in columns:
+        if column not in headers:
+            exit()
+    if ['id', 'name', 'price'] in headers:
         print('The file does not have the right header columns. Please provide format: id, name, price')
 
-header_check(data1)
+headerCheck(data1)
 
 def file_copy(data_file1,data_file2):
     # Check if id is present in file2
